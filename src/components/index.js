a7a8369a133c569791f1e6e56901b071bf30df0b
nhudack <nhudack@vt.edu>
2021-08-15 11:52:14 -0400

    Simplified website design

--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,6 @@ export { default as Loader } from './loader';
 export { default as Nav } from './nav';
 export { default as Menu } from './menu';
 export { default as Side } from './side';
-export { default as Social } from './social';
-export { default as Footer } from './footer';
 export { default as Hero } from './sections/hero';
 export { default as About } from './sections/about';
 //export { default as Jobs } from './sections/jobs';
