581c2bdcb7231888e57e1a4669f9b4c16afc1d96
nhudack <nhudack@vt.edu>
2021-08-15 10:34:10 -0400

    Redesigned website

---
+++ src/components/sections/contact.js
@@ -0,0 +1,112 @@
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components';
+import { srConfig, email } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+import { Icon } from '@components/icons';
+import { socialMedia } from '@config';
+
+const StyledContactSection = styled.section`
+  max-width: 600px;
+  margin: 0 auto 100px;
+  text-align: center;
+  color: var(--dark-slate);
+
+  @media (max-width: 768px) {
+    margin: 0 auto 50px;
+  }
+
+  .overline {
+    display: block;
+    margin-bottom: 20px;
+    color: var(--green);
+    font-family: var(--font-mono);
+    font-size: var(--fz-md);
+    font-weight: 400;
+
+    &:before {
+      bottom: 0;
+      font-size: var(--fz-sm);
+    }
+
+    &:after {
+      display: none;
+    }
+  }
+
+  .title {
+    font-size: clamp(40px, 5vw, 60px);
+  }
+
+  .email-link {
+    ${({ theme }) => theme.mixins.bigButton};
+    margin-top: 50px;
+  }
+`;
+
+const StyledSocialLinks = styled.div`
+  display: block;
+  width: 100%;
+  max-width: 270px;
+  margin: 0 auto 10px;
+  color: var(--dark-slate);
+
+  ul {
+    ${({ theme }) => theme.mixins.flexBetween};
+    padding: 0;
+    margin: 0;
+    list-style: none;
+
+    a {
+      padding: 10px;
+      svg {
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
+`;
+
+const Contact = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  return (
+    <StyledContactSection id="contact" ref={revealContainer}>
+      <h2 className="numbered-heading">Contact Me</h2>
+
+      <h2 className="title">Contact Me</h2>
+
+      <p>
+        Let's connect! You can find me on almost any social media.
+      </p>
+
+      <a className="email-link" href={`mailto:${email}`}>
+        Email
+      </a>
+
+      <StyledSocialLinks>
+        <ul>
+          {socialMedia &&
+            socialMedia.map(({ name, url }, i) => (
+              <li key={i}>
+                <a href={url} aria-label={name}>
+                  <Icon name={name} />
+                </a>
+              </li>
+            ))}
+        </ul>
+      </StyledSocialLinks>
+    </StyledContactSection>
+  );
+};
+
+export default Contact;
