0f2fd21626238b504d6d53cebc408e3b45915df5
nhudack <nhudack@vt.edu>
2021-08-18 23:46:28 -0400

    Added background image

--- src/components/sections/home.js
+++ src/components/sections/home.js
@@ -11,7 +11,7 @@ const StyledHomeSection = styled.section`
   min-height: 100vh;
   padding: 0;
 
-  background-image: url("/images/background.jpg");
+  background-image: url("/images/background.png");
   height: 100vh;
   background-position: center;
   background-repeat: no-repeat;
