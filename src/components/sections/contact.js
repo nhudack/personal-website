ba97033db3c5a06a730b676ce9dfc1e1b0230091
nhudack <nhudack@vt.edu>
2021-08-18 22:50:59 -0400

    Reverted changes

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -16,24 +16,6 @@ const StyledContactSection = styled.section`
     margin: 0 auto 50px;
   }
 
-  .overline {
-    display: block;
-    margin-bottom: 20px;
-    color: var(--green);
-    font-family: var(--font-mono);
-    font-size: var(--fz-md);
-    font-weight: 400;
-
-    &:before {
-      bottom: 0;
-      font-size: var(--fz-sm);
-    }
-
-    &:after {
-      display: none;
-    }
-  }
-
   .title {
     font-size: clamp(40px, 5vw, 60px);
   }
