a538fd593edcae56f8eab08d058f0ad0c83c3c72
nhudack <nhudack@vt.edu>
2021-08-19 21:27:29 -0400

    Updated Projects section

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -14,11 +14,11 @@ const StyledProjectsSection = styled.section`
   margin-top: -120px;
 
   @media (max-width: 768px) {
-    margin-top: -40;
+    margin-top: -20;
   }
 
   @media (max-width: 480px) {
-    margin-top: -20;
+    margin-top: -10;
   }
 
   h2 {
