375ba36d7c8751a0222dcfcfb285f1e020295e8b
nhudack <nhudack@vt.edu>
2021-08-19 20:55:51 -0400

    Updated Projects section

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -22,7 +22,6 @@ const StyledProjectsSection = styled.section`
     grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
     grid-gap: 15px;
     position: relative;
-    margin-top: 50px;
 
     @media (max-width: 1080px) {
       grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
