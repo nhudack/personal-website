9adff1d6e0db5dee19dca032887e43304cfa4790
nhudack <nhudack@vt.edu>
2021-08-20 11:10:13 -0400

    Updated hero image

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -9,13 +9,12 @@ const StyledHeroBackground = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
-  margin: 0 -50vw;
+  margin: 0 0;
   min-width: 100vw;
   min-height: 100vh;
 
   position: relative;
-  left: 50%;
-  right: 50%;
+  left: calc(-50vw + 50%)
 `;
 
 const StyledHeroSection = styled.section`
