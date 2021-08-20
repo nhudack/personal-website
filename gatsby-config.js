5933c39afcb59e8badc9ff7a68bc81ee7595ee09
nhudack <nhudack@vt.edu>
2021-08-20 14:00:34 -0400

    Updated config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         start_url: '/',
         background_color: config.colors.white,
         theme_color: config.colors.navy,
+        // display: 'minimal-ui',
         icon: 'src/images/logo.png',
       },
     },
