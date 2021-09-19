5b01c454c405b37697757aaebd730148f4e01f4d
nhudack <nhudack@vt.edu>
2021-09-19 17:20:33 -0400

    Updated social media section

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -8,7 +8,6 @@ export { default as Icon } from './icon';
 export { default as IconGitHub } from './github';
 export { default as IconInstagram } from './instagram';
 export { default as IconLinkedin } from './linkedin';
-export { default as IconLoader } from './loader';
 export { default as IconMail } from './mail';
 export { default as IconPlayStore } from './playstore';
 export { default as IconStar } from './star';
