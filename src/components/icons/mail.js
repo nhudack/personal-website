a7720fddecbf5929a04dc5418c37402593c8c282
nhudack <nhudack@vt.edu>
2021-09-19 17:27:42 -0400

    Updated contact section

--- src/components/icons/mail.js
+++ src/components/icons/mail.js
@@ -8,8 +8,8 @@ const IconMail = () => (
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    //strokeLinecap="round"
+    //strokeLinejoin="round"
     className="feather feather-mail">
     <title>Mail</title>
     <path d="M30,60 C46.5685433,60 60,46.5685433 60,30 C60,13.4314567 46.5685433,0 30,0 C13.4314567,0 0,13.4314567 0,30 C0,46.5685433 13.4314567,60 30,60 Z"></path>
