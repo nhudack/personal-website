b4a62726ce8c8555e9652cbaf1bbf47518012f69
nhudack <nhudack@vt.edu>
2021-09-19 20:20:25 -0400

    Updated icons

--- src/components/icons/resume.js
+++ src/components/icons/resume.js
@@ -9,12 +9,12 @@ const IconResume = () => (
     className="feather feather-resume">
     <title>Resume</title>
     <path d="M30,60 C46.5685433,60 60,46.5685433 60,30 C60,13.4314567 46.5685433,0 30,0 C13.4314567,0 0,13.4314567 0,30 C0,46.5685433 13.4314567,60 30,60 Z" fill="currentColor"></path>
-    <rect stroke="currentColor" rx="1" id="svg_2" height="39" width="30" y="10.5" x="15" fill="var(--white)"/>
-    <line id="svg_22" y2="17" x2="35" y1="17" x1="25" stroke-width="3" stroke="currentColor" fill="none"/>
-    <line id="svg_23" y2="24" x2="39" y1="24" x1="21" stroke-width="2" stroke="currentColor" fill="none"/>
-    <line id="svg_31" y2="30" x2="39" y1="30" x1="21" stroke-width="2" stroke="currentColor" fill="none"/>
-    <line id="svg_32" y2="36" x2="39" y1="36" x1="21" stroke-width="2" stroke="currentColor" fill="none"/>
-    <line id="svg_33" y2="42" x2="39" y1="42" x1="21" stroke-width="2" stroke="currentColor" fill="none"/>
+    <rect stroke="currentColor" rx="1" height="36" width="28" y="12" x="16" fill="var(--white)"/>
+    <line y2="18" x2="35" y1="18" x1="25" stroke-width="2" stroke="currentColor" fill="none"/>
+    <line y2="25" x2="39" y1="25" x1="21" stroke="currentColor" fill="none"/>
+    <line y2="35" x2="39" y1="35" x1="21" stroke="currentColor" fill="none"/>
+    <line y2="40" x2="39" y1="40" x1="21" stroke="currentColor" fill="none"/>
+    <line y2="30" x2="39" y1="30" x1="21" stroke="currentColor" fill="none"/>
   </svg>
 );
 
