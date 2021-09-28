eae4e2323ccb30d13fe1a5ab87cae772d5b19927
nhudack <nhudack@vt.edu>
2021-09-28 15:48:18 -0400

    Removed featured section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Layout, Hero, About, Experience, Featured, Projects, Contact } from '@components';
+import { Layout, Hero, About, Experience, Projects, Contact } from '@components';
 
 const StyledMainContainer = styled.main`
   counter-reset: section;
